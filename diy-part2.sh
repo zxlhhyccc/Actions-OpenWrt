ae78d4f87021a1c78c5f1c483c8b1be3825f1b42
zxlhhyccc <zxlhhy@gmail.com>
2020-12-27 23:10:13 +0800

    diy-part2.sh:add NanoPi R4S support

--- diy-part2.sh
+++ diy-part2.sh
@@ -278,9 +278,11 @@ popd
 # mac80211：为ath / subsys：在2g上允许vht添加补丁
 wget -P ./package/kernel/mac80211/patches/subsys/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/package/kernel/mac80211/patches/subsys/600-mac80211-allow-vht-on-2g.patch
 wget -P ./package/kernel/mac80211/patches/ath/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/package/kernel/mac80211/patches/ath/983-ath10k-allow-vht-on-2g.patch
-# 修正友善补丁
-# rm -rf target/linux/rockchip/patches-5.4
-# svn co https://github.com/zxlhhyccc/openwrt-master/trunk/target/linux/rockchip/patches-5.4 target/linux/rockchip/patches-5.4
+# 修正友善补丁(R2S/R4S)
+rm -rf package/boot/uboot-rockchip
+svn co https://github.com/project-openwrt/openwrt/branches/master/package/boot/uboot-rockchip package/boot/uboot-rockchip
+rm -rf target/linux/rockchip
+svn co https://github.com/project-openwrt/openwrt/branches/master/target/linux/rockchip target/linux/rockchip
 # busybox：为docker top命令添加ps -ef选项的补丁
 wget -P ./package/utils/busybox/patches/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/package/utils/busybox/patches/900-add-e-f-option-for-docker.patch
 
