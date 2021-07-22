8418c069f0c6de707ec3219b931208c8014031d6
zxlhhyccc <zxlhhy@gmail.com>
2021-07-22 14:05:03 +0800

    diy-part2.sh:add 683-patch

--- diy-part2.sh
+++ diy-part2.sh
@@ -169,6 +169,8 @@ rm -f ./target/linux/generic/hack-5.4/661-use_fq_codel_by_default.patch
 wget -P ./target/linux/generic/hack-5.4/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/target/linux/generic/hack-5.4/661-use_fq_codel_by_default.patch
 rm -f ./target/linux/generic/hack-5.4/662-remove_pfifo_fast.patch
 wget -P ./target/linux/generic/hack-5.4/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/target/linux/generic/hack-5.4/662-remove_pfifo_fast.patch
+rm -f ./target/linux/generic/hack-5.4/683-NET-add-mac-address-property.patch
+wget -P ./target/linux/generic/hack-5.4/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/target/linux/generic/hack-5.4/683-NET-add-mac-address-property.patch
 rm -f ./target/linux/generic/hack-5.4/721-phy_packets.patch
 wget -P ./target/linux/generic/hack-5.4/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/target/linux/generic/hack-5.4/721-phy_packets.patch
 wget -P target/linux/generic/hack-5.4/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/target/linux/generic/hack-5.4/952-net-conntrack-events-support-multiple-registrant.patch
