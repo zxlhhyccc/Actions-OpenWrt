f768e57e2028a26f47650d87fa221f62678ef339
zxlhhyccc <zxlhhyccc@gmail.com>
2022-03-29 22:47:08 +0800

    image: let mksquashfs4 use all processors

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 # squashfs：使mkfs具有多CPU加速和添加固件日期形式的编号
-sed -i 's/processors 1/processors $(shell nproc)/g' include/image.mk
+# sed -i 's/processors 1/processors $(shell nproc)/g' include/image.mk
 sed -i '35i BUILD_DATE_PREFIX := $(shell date +'%Y%m%d%H%M')' include/image.mk
 sed -i "s/%Y%m%d%H%M/\'%Y%m%d%H%M\'/g" include/image.mk
 sed -i 's/$(VERSION_DIST_SANITIZED)/$(BUILD_DATE_PREFIX)-$(VERSION_DIST_SANITIZED)/g' include/image.mk
