cf92de4b735737cad94741a446a6b531c5e503f7
zxlhhyccc <zxlhhyccc@gmail.com>
2022-04-10 18:24:43 +0800

    diy-part1.sh: fix 8 timezone

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 # squashfs：使mkfs具有多CPU加速和添加固件日期形式的编号
 # sed -i 's/processors 1/processors $(shell nproc)/g' include/image.mk
-sed -i '35i BUILD_DATE_PREFIX := $(shell date +'%Y%m%d%H%M')' include/image.mk
+sed -i '35i BUILD_DATE_PREFIX := $(shell TZ=UTC-8 date +'%Y%m%d%H%M')' include/image.mk
 sed -i "s/%Y%m%d%H%M/\'%Y%m%d%H%M\'/g" include/image.mk
 sed -i 's/$(VERSION_DIST_SANITIZED)/$(BUILD_DATE_PREFIX)-$(VERSION_DIST_SANITIZED)/g' include/image.mk
 # openssl：通过以下方式，使ARMv8设备适配ChaCha20-Poly1305而不是AES-GCM
