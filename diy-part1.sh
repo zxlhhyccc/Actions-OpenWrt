b1952c871ce8214070752cf406ce8780a3338b95
zxlhhyccc <zxlhhyccc@gmail.com>
2022-07-24 14:39:04 +0800

    fix x86 bios boot partition is under 1 MiB

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,8 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+# fix bios boot partition is under 1 MiB
+sed -i 's/256/1024/g' target/linux/x86/image/Makefile
 # squashfs：使mkfs具有多CPU加速和添加固件日期形式的编号
 # sed -i 's/processors 1/processors $(shell nproc)/g' include/image.mk
 sed -i '35i BUILD_DATE_PREFIX := $(shell TZ=UTC-8 date +'%Y%m%d%H%M')' include/image.mk
